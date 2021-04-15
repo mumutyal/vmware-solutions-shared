eb6cde2cd46d46fc876ec20e9a44295d4bf6c407
Murali krishna <mumutyal@in.ibm.com>
2021-04-15 07:51:07 -0700

    multi vms

--- output.tf
+++ output.tf
@@ -5,12 +5,10 @@ output "virtual_machine_access" {
       ssh root@${module.ibm_vmware_solutions_shared_instance.default_external_network_ip} 
       
       The initial password is: ${vcd_vapp_vm.vm_2.customization[0].admin_password} which you will need to change on first login. 
-  VM
   ### You can access the vm instance ${vcd_vapp_vm.vm_3.name} using the following SSH command:
       ssh root@${module.ibm_vmware_solutions_shared_instance.default_external_network_ip} 
       
       The initial password is: ${vcd_vapp_vm.vm_3.customization[0].admin_password} which you will need to change on first login. 
-  VM
   ### You can access the vm instance ${vcd_vapp_vm.vm_4.name} using the following SSH command:
       ssh root@${module.ibm_vmware_solutions_shared_instance.default_external_network_ip} 
       
